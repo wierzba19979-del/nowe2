cc5f3c8d4702220627f751c0b4db28beaaca5abc
Rafał <wierzba19979@gmail.com>
2026-09-21 13:52:36 +0200

    PD-4: Poprawa klasy Resoruce

--- src/main/java/pd4/Resource.java
+++ src/main/java/pd4/Resource.java
@@ -3,9 +3,7 @@ package pd4;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
-
 @SuperBuilder
-@AllArgsConstructor
 @Getter
 public abstract sealed class Resource implements Comparable<Resource> permits Book, Movie{
     private final int id;
